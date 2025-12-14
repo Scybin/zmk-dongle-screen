47fff525da16670005b4d02f5efa4ce565560405
Scybin <xheavyice@gmail.com>
2025-12-13 19:44:47 -0700

    Include lvgl

--- boards/shields/dongle_screen/src/widgets/battery_status.c
+++ boards/shields/dongle_screen/src/widgets/battery_status.c
@@ -17,6 +17,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include <zmk/events/usb_conn_state_changed.h>
 #include <zmk/event_manager.h>
 #include <zmk/usb.h>
+#include <lvgl.h>
 #include <fonts.h> // NerdFont
 
 #include "battery_status.h"
